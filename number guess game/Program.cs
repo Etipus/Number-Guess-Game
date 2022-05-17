ac7b1d73699ff85027cea4b31ccec3d42acc28bf
Enes <etipuss@gmail.com>
2022-05-17 13:54:49 +0300

    Add project files.

---
+++ number guess game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_guess_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            game();
+        }
+        static void game()
+        {
+            bool dogrumu = false;
+
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 11);
+
+            Console.WriteLine("Welcome to the number guessing game!");
+
+            while (!dogrumu)
+            {
+                Console.WriteLine("Guess a number between 1 and 10:");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin < randomnumber)
+                    Console.WriteLine("Try a higher number");
+                else if (tahmin > randomnumber)
+                    Console.WriteLine("Try a lower number");
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine("Conqratulations, you have won the game!\nIf you wanna play the game again you can press 't' if you wanna quit from the game you can press anything except 't'");
+                    char secim = Convert.ToChar(Console.ReadLine());
+                    if (secim == 't')
+                        game();
+                    else
+                        Environment.Exit(0);
+                }
+    }
+}
+    }
+}
+
+
+
+
